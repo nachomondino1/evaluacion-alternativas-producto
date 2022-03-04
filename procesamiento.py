418f954a300e3d59164d8bca0ca1d0405c26c4f1
nachomondino1 <nachomondino1@gmail.com>
2022-03-04 18:02:26 -0300

    Cree Clase "MercadoLibreCrawler" y le mande todo lo que tenia LinksExtractor.py, OpinionsExtractor.py y PublicationExtractor.py... Desarrolle el codigo de procesamiento.py que llamara a los metodos de MercadoLibreCrawler.py para hacer el webscraping pero tambien llamara al limpiador de texto y demas

--- procesamiento.py
+++ procesamiento.py
@@ -16,3 +16,91 @@ Pseudocodigo de lo que quisiera que haga:
     atributo pues tamaño de pantalla tiene menos relacion con  “el celular le dura todo el dia” que el atributo bateria por lo que, no deberian tener 4,3 los dos..)
 """
 
+# Importo librerias
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from MercadoLibreCrawler import MercadoLibreCrawler
+
+# Faltaria importar browser_crawler para scroll down
+
+
+# Defino a Chrome como Web Browser
+opts = Options()
+opts.add_argument(
+    "USER_AGENT=Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3")
+driver = webdriver.Chrome('/Users/nachomondino/Desktop/chromedriver', chrome_options=opts)
+
+
+def unirDataFrames(df1,df2):
+    df = pd.concat([df1, df2])
+    return df
+
+
+def main():
+    df_opiniones_publicaciones = pd.DataFrame(columns=['title', 'content', 'rate', 'likes', 'dislikes'])
+    paginacion_num, paginacion_max = 1, 10
+
+    # luego implementare que busqueda = 20/30 prod mas demandados
+    busqueda = str(input("Ingrese producto: "))
+    # busqueda = "celulares"
+
+    crawler = MercadoLibreCrawler(driver, busqueda)
+
+    # Valido la busqueda (para que no sea tan amplia)
+    while crawler.validacionBusqueda(driver,busqueda) == None:
+        print("Por favor sea mas especifico en su busqueda")
+        busqueda = str(input("Ingrese busqueda: "))
+
+
+    # Ingreso a HomePage
+    driver.get(crawler.getHomePageUrl(busqueda))
+
+    while paginacion_num < paginacion_max:
+
+        # Extraigo links
+        links_publicaciones = crawler.getPublicationsUrl(driver)
+        link_paginacion = crawler.getPaginacionUrl(driver)
+
+        for publicacion in links_publicaciones[:3]:
+
+            # Ingreso a publicacion
+            driver.get(publicacion)
+
+            # Click en "Ver todas las opiniones" --> Si no puede hacer click es por dos razones: 1) o no hay opiniones 2) hay menos de 5 opiniones. En cualquier caso me conviene no obtenerlas
+            if crawler.ClickVerTodasLasOpiniones(driver) == True:
+
+                # Verifico Opiniones Repetidas
+                if crawler.verificationNewOpinions(driver, df_opiniones_publicaciones) == True:
+                    # Hago Scroll down para cargar todas las opiniones (pues son nuevas y las quiero extraer)
+                    # WebScrapingActions.ScrollDown(driver)
+
+                    # Extraigo opiniones
+                    df_opiniones_publicacion = crawler.getPublicacionOpinions(driver)
+                    print(df_opiniones_publicacion)
+                    df_opiniones_publicaciones = unirDataFrames(df_opiniones_publicaciones, df_opiniones_publicacion)
+
+                    # Extraigo descripcion del producto (notar que solo lo extraigo si las opiniones son nuevas)
+                    driver.back()  # salgo de "ver todas las opiniones"
+
+                    # implementar extraccion
+
+                else:
+                    # Vuelvo a pagina de publicacion
+                    driver.back()
+
+            else:
+                pass
+
+            # Vuelvo a Home Page
+            driver.back()  # Puede que no haga falta
+
+        # Hago click en Siguiente pagina  (guardo paginas visitadas?)
+        driver.get(link_paginacion)
+        print("Cambio de pagina", link_paginacion)
+        paginacion_num += 1
+
+    driver.close()
+
+main()
+
