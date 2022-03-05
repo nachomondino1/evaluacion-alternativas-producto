cf29b22c225fcae9d2d2e4057aef34eeb5727529
nachomondino1 <nachomondino1@gmail.com>
2022-03-05 17:13:07 -0300

    Organize la estructura del proyecto - Agregue directorio “útil” donde guardare todos los archivos necesarios para el código del proyecto. En particular agregue la carpeta “web_scraping” que tiene el archivo crawler.py que es donde guardo la clase Crawler() quien es padre de la clase MercadoLibreCrawler(). Todos los métodos que haga para la clase MercadoLibreCrawler() que sirvan no solo para meli sino en general en cualquier proyecto los pasare a la clase Crawler(). -  Agregue archivo MercadoLibreApi.py pues utilizare la API de meli para algunas cosas como por ejemplo obtener las categorías de los productos
    En web Scraping main.py:
    - Añadi metodo getidProducto()
    - Cree función OpinionsDataframe
    
    Creación de MercadoLibreApi.py en donde esta la clase MercadoLibreApi()
    - Cree metodo def getIdCategorias(self):
    - Cree metodo def getAtributosObligatorios(self, id_categoria):
    - Cree metodo def getOpinions(self):

--- procesamiento.py
+++ procesamiento.py
@@ -1,12 +1,12 @@
 """
 Pseudocodigo de lo que quisiera que haga:
-# Para una búsqueda de muchas búsquedas: # que busquedas? Podria guardar una lista de los 20/30 productos mas demandados por los clientes y tener esos datos ya preparados. El resto de las busquedas se podrian hacer en el momento llamando a procesamiento.py y le podria mostrar un relojito al cliente porque va a tardar.
-    1.- Extrae los datos mediante web scraping para esa busqueda (uso directorio de web scraping)
+# Para una búsqueda de muchas búsquedas: # que busquedas? Podria guardar una lista de los 20/30 productos mas demandados por los clientes y tener esos datos ya preparados. El resto de las busquedas se podrian hacer en el momento llamando a main.py y le podria mostrar un relojito al cliente porque va a tardar.
+    1.- Extrae los datos mediante web scraping para esa busqueda o los busca (uso directorio de web scraping)
+        1.1.-Busca atributos (pues necesito saber a que atributos debo extraerle su valor en cada publicacion.
     2.- Limpio el texto (uso directorio de text mining)
         2.1 Extrae Customer needs (a partir de analisis de opiniones)
-    3. Busca atributos
-    4. Me pide matriz de relaciones
-    5. Sentiment Analysis
+    3. Me pide matriz de relaciones
+    4. Sentiment Analysis
     (necesito matriz de relaciones si o si para linkear el sentiment de las customer needs a cada valor de cada atributo).
     Por ej, si una opinion dice que el celular le dura
     todo el dia entonces se relacion con la bateria y el tamaño de pantalla del celular (en menor medida) y le debere
@@ -16,104 +16,30 @@ Pseudocodigo de lo que quisiera que haga:
     atributo pues tamaño de pantalla tiene menos relacion con  “el celular le dura todo el dia” que el atributo bateria por lo que, no deberian tener 4,3 los dos..)
 """
 
-# Importo librerias
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from MercadoLibreCrawler import MercadoLibreCrawler
-
-# Faltaria importar browser_crawler para scroll down
+# Importo Librerias
+from meli_web_scraping.MercadoLibreApi import MercadoLibreApi
 
 
-# Defino a Chrome como Web Browser
-opts = Options()
-opts.add_argument(
-    "USER_AGENT=Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3")
-driver = webdriver.Chrome('/Users/nachomondino/Desktop/chromedriver', chrome_options=opts)
+# 1.- Extrae los datos mediante web scraping para esa busqueda o los busca (uso directorio de web scraping)
+# 1.1.- Busca atributos
+extractor = MercadoLibreApi()
 
-"""
-def unirDataFrames(df1,df2):
-    df = pd.concat([df1, df2])
-    return df
-"""
+# # para la busqueda del usuario, extraer id_categoria (del link) y llamar a la siguiente funcion
+# # Ejs de codigos: MLA1055 (para celulares) ; MLA1002 (no se de que es)
+# # DOS ALTERNATIVAS: con API o con web Scraping...
 
-def OpinionsDataFrame(new_opinions, df):
-    """
-    Mi idea es hacer una funcion que cree el dataframe
-    :return:
-    """
-    # Creo diccionario con nuevas opiniones y luego lo convierto a dataFrame
-    d = {'title':new_opinions[0], 'content':new_opinions[1], 'rate':new_opinions[2], 'likes':new_opinions[3], 'dislikes':new_opinions[4]}
-    new_df = pd.DataFrame(data=d)
+extractor.getIdCategorias()
 
-    # Concateno los dataframe de opiniones juntando las viejas y las nuevas
-    df = pd.concat([df, new_df])
-    return df
+extractor.getAtributosObligatorios("MLA3502")
 
 
-def main():
-    df_opiniones_publicaciones = pd.DataFrame(columns=['title', 'content', 'rate', 'likes', 'dislikes'])
-    paginacion_num, paginacion_max = 1, 10
 
-    # luego implementare que busqueda = 20/30 prod mas demandados
-    # busqueda = str(input("Ingrese producto: "))
-    busqueda = "celulares"
-
-    crawler = MercadoLibreCrawler(driver, busqueda)
-
-    # Valido la busqueda (para que no sea tan amplia)
-    while crawler.validacionBusqueda(driver,busqueda) == None:
-        print("Por favor sea mas especifico en su busqueda")
-        busqueda = str(input("Ingrese busqueda: "))
+# 2.- Limpio el texto (uso directorio de text mining)
 
 
-    # Ingreso a HomePage
-    driver.get(crawler.getHomePageUrl(busqueda))
+# 3. Me pide matriz de relaciones
 
-    while paginacion_num < paginacion_max:
 
-        # Extraigo links
-        links_publicaciones = crawler.getPublicationsUrl(driver)
-        link_paginacion = crawler.getPaginacionUrl(driver)
 
-        for publicacion in links_publicaciones[:3]:
-
-            # Ingreso a publicacion
-            driver.get(publicacion)
-
-            # Click en "Ver todas las opiniones" --> Si no puede hacer click es por dos razones: 1) o no hay opiniones 2) hay menos de 5 opiniones. En cualquier caso me conviene no obtenerlas
-            if crawler.ClickVerTodasLasOpiniones(driver) == True:
-
-                # Verifico Opiniones Repetidas
-                if crawler.verificationNewOpinions(driver, df_opiniones_publicaciones) == True:
-                    # Hago Scroll down para cargar todas las opiniones (pues son nuevas y las quiero extraer)
-                    # WebScrapingActions.ScrollDown(driver)
-
-                    # Extraigo opiniones
-                    opiniones_publicacion = crawler.getPublicacionOpinions(driver)
-                    df_opiniones_publicaciones = OpinionsDataFrame(opiniones_publicacion, df_opiniones_publicaciones)
-                    print(df_opiniones_publicaciones)
-                    # Extraigo descripcion del producto (notar que solo lo extraigo si las opiniones son nuevas)
-                    driver.back()  # salgo de "ver todas las opiniones"
-
-                    # implementar extraccion
-
-                else:
-                    # Vuelvo a pagina de publicacion
-                    driver.back()
-
-            else:
-                pass
-
-            # Vuelvo a Home Page
-            driver.back()  # Puede que no haga falta
-
-        # Hago click en Siguiente pagina  (guardo paginas visitadas?)
-        driver.get(link_paginacion)
-        print("Cambio de pagina", link_paginacion)
-        paginacion_num += 1
-
-    driver.close()
-
-main()
+# 4. Sentiment Analysis
 
