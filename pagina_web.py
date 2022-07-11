8ce139f4d3fa26827e878fc7e576877f97dd0474
nachomondino1 <nachomondino1@gmail.com>
2022-07-11 16:01:29 -0300

    Ultimos cambios tras informe

--- pagina_web.py
+++ pagina_web.py
@@ -230,7 +230,8 @@ def set_customer_needs_weigths(df_cust_needs, product):
     for i in range(len(l_cust_needs_three_words)):
 
         # Defino variables
-        label = '*{}) {}:*'.format(i + 1, l_cust_needs_three_words[i].upper())  # titulo de cada slider
+        st.subheader('{}) {}:'.format(i + 1, l_cust_needs_three_words[i].upper()))  # titulo de cada slider
+        label = '{}) {}:'.format(i + 1, l_cust_needs_three_words[i].upper())  # titulo de cada slider
         help = get_help_button(l_cust_needs_one_word[i], product)  # Texto help de cada slider
 
         # SI EL CLIENTE NO SELECCIONO UN USO, O BIEN, EL PRODUCTO NO LOS TIENE ESPECIFICADOS
