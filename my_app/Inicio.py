5d0c98cd739c9b003544f29202bb90ddbca9ad4a
nachomondino1 <nachomondino1@gmail.com>
2022-09-19 17:11:59 -0300

    Cambios leves (ortografia) en diseño en Pagina web

--- my_app/Inicio.py
+++ my_app/Inicio.py
@@ -14,7 +14,7 @@ def main():
                 "que les sirva tanto como me sirvió a mí. Pueden contactarme en el siguiente mail: nachomondino1@gmail.com")
 
     # Imprimo titulo
-    st.header('COMPRAR NUNCA FUE TAN FÁCIL')  # st.title('EVALUACION AUTOMATICA DE ALTERNATIVAS EN PROCESO DE COMPRA')
+    st.title('COMPRAR NUNCA FUE TAN FÁCIL')  # st.title('EVALUACION AUTOMATICA DE ALTERNATIVAS EN PROCESO DE COMPRA')
 
     # ESCRIBO INTRODUCCION AL PROBLEMA QUE RESUELVE LA HERRAMIENTA
     st.write("Quedate con la tranquilidad de elegir la opción óptima de un producto ahorrando mucho tiempo y estrés.")
@@ -22,15 +22,24 @@ def main():
     st.image(image_0, use_column_width=True)
     st.markdown("---")
 
-    st.write("Actualmente, disponemos de dos herramientas que facilitan la elección de una alternativa en la compra de "
-             "un producto. Éstas son:")
-    image_1 = Image.open('./p5_deployment/utils/alternativas_posibles.png')
-    image_2 = Image.open('./p5_deployment/utils/posicion_mercado.jpeg')
-    col1, col2, col3, col4, col5 = st.columns([0.2, 3.19, 0.2, 4, 0.2])
-    col2.image(image_1, use_column_width=True, caption="Evaluador de alternativas")
-    col4.image(image_2, use_column_width=True, caption="Evaluador de marcas")
+    st.header("Herramientas")
+    st.write("Actualmente, se dispone de dos herramientas que facilitan el proceso de compra de un producto. Éstas son:")
+    tab1, tab2 = st.tabs(["Evaluador de alternativas", "Evaluador de marcas"])
+
+    with tab1:
+        st.header("A cat")
+        image_1 = Image.open('./p5_deployment/utils/alternativas_posibles.png')
+        st.image(image_1, use_column_width=True, caption="Evaluador de alternativas")
+
+
+    with tab2:
+        st.header("A dog")
+        image_2 = Image.open('./p5_deployment/utils/posicion_mercado.jpeg')
+        st.image(image_2, use_column_width=True, caption="Evaluador de marcas")
+
     st.write(" ")
 
+    st.header("Productos")
     st.write("A su vez, los productos relevados son:")
     image_3 = Image.open('./p5_deployment/utils/celular.jpeg')
     image_4 = Image.open('./p5_deployment/utils/smartband.jpeg')
