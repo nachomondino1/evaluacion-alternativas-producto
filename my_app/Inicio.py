cbe48ebb1ab97a26cc1e0a9272d27cdce8aee4a5
nachomondino1 <nachomondino1@gmail.com>
2022-09-19 17:20:37 -0300

    Cambios leves (ortografia) en diseño en Pagina web

--- my_app/Inicio.py
+++ my_app/Inicio.py
@@ -17,7 +17,7 @@ def main():
     st.title('COMPRAR NUNCA FUE TAN FÁCIL')  # st.title('EVALUACION AUTOMATICA DE ALTERNATIVAS EN PROCESO DE COMPRA')
 
     # ESCRIBO INTRODUCCION AL PROBLEMA QUE RESUELVE LA HERRAMIENTA
-    st.write("Quedate con la tranquilidad de elegir la opción óptima de un producto ahorrando mucho tiempo y estrés.")
+    st.write("Hace que comprar un producto sea rápido y fácil")
     image_0 = Image.open('./p5_deployment/utils/prueba.png')
     st.image(image_0, use_column_width=True)
     st.markdown("---")
