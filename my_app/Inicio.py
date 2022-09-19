862f08f710d95a330b1bfc1ff482f741d9a23c50
nachomondino1 <nachomondino1@gmail.com>
2022-09-19 17:18:52 -0300

    Cambios leves (ortografia) en diseño en Pagina web

--- my_app/Inicio.py
+++ my_app/Inicio.py
@@ -27,16 +27,18 @@ def main():
     tab1, tab2 = st.tabs(["Evaluador de alternativas", "Evaluador de marcas"])
 
     with tab1:
+        st.header("Evaluador de alternativas de un producto")
         st.write("Queres comprar un celular y no sabes que opción elegir? O tal vez un televisor y te estresa que haya "
                  "tantos modelos? En estos casos, te recomendamos usar esta herramienta!")
         image_1 = Image.open('./p5_deployment/utils/alternativas_posibles.png')
-        st.image(image_1, use_column_width=True, caption="Evaluador de alternativas", width=200)
+        st.image(image_1, use_column_width=True, caption="Evaluador de alternativas", width=100)
 
     with tab2:
+        st.header("Evaluador de marcas de un producto")
         st.write("Queres ver que marca es la mejor en un producto como los celulares? Si es así, te recomendamos "
                  "usar esta herramienta!")
         image_2 = Image.open('./p5_deployment/utils/posicion_mercado.jpeg')
-        st.image(image_2, use_column_width=True, caption="Evaluador de marcas", width=200)
+        st.image(image_2, use_column_width=True, caption="Evaluador de marcas", width=100)
 
     st.write(" ")
 
